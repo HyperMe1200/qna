b48188b44c7cc7b415f488b1fe7b3b2e07681005
Andrey Pridatchenko <andrey.pridatchenko@rcntec.com>
2017-05-12 12:56:13 +0300

    Migration of answers table

--- db/migrate/20170512094757_create_answers.rb
+++ db/migrate/20170512094757_create_answers.rb
@@ -1,7 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
     create_table :answers do |t|
-
+      t.text :body
+      t.belongs_to :question, index: true
       t.timestamps
     end
   end
