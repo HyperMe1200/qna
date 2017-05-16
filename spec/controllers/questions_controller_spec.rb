916a0d73186d1c79d9f5087f72d90a92dc780f00
Andrey Pridatchenko <andrey.pridatchenko@rcntec.com>
2017-05-16 14:40:07 +0300

    Question show test and action

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :index
     end
   end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: question } }
+    it 'assigns requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
+
 end
