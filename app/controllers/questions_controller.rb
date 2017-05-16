fc7c4649a058d186de10d51dad84d1e2b81860cd
Andrey Pridatchenko <andrey.pridatchenko@rcntec.com>
2017-05-16 22:16:44 +0300

    Question destroy test and action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: [:update, :show, :edit]
+  before_action :set_question, only: [:destroy, :update, :show, :edit]
 
   def index
     @questions = Question.all
@@ -29,6 +29,11 @@ class QuestionsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_path
   end
 
   private
