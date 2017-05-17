48d019e329922d64c3dc3f2be332fca21cac40ec
Andrey Pridatchenko <andrey.pridatchenko@rcntec.com>
2017-05-17 10:01:01 +0300

    Answers new test and action

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -60,7 +60,6 @@ RSpec.describe QuestionsController, type: :controller do
       it 'does not save the question' do
         expect { post :create, params: { question: attributes_for(:invalid_question) } }.to_not change(Question, :count)
       end
-
       it 're-renders new view' do
         post :create, params: { question: attributes_for(:invalid_question) }
         expect(response).to render_template :new
@@ -74,7 +73,6 @@ RSpec.describe QuestionsController, type: :controller do
         patch :update, params: { id: question, question: attributes_for(:question) }
         expect(assigns(:question)).to eq question
       end
-
       it 'change question attributes' do
         patch :update, params: { id: question, question: { title: 'new_title', body: 'new_body' } }
         question.reload
@@ -97,7 +95,6 @@ RSpec.describe QuestionsController, type: :controller do
         expect(question.title).to eq 'MyString'
         expect(question.body).to eq 'MyText'
       end
-
       it 're-renders edit view' do
         expect(response).to render_template :edit
       end
@@ -107,10 +104,8 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'DELETE #destroy' do
     before { question }
     it 'deletes question' do
-      expect { delete :destroy, params: { id: question } }
-          .to change(Question, :count).by(-1)
+      expect { delete :destroy, params: { id: question } }.to change(Question, :count).by(-1)
     end
-
     it 'redirects to index view' do
       delete :destroy, params: { id: question }
       expect(response).to redirect_to questions_path
