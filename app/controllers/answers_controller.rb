5f140c84146007e446f6ade031ff6a2f717d111a
Andrey Pridatchenko <andrey.pridatchenko@rcntec.com>
2017-05-24 18:06:51 +0300

    Author can delete answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,6 +4,7 @@ class AnswersController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
     @answer = @question.answers.new(answer_params)
+    @answer.user = current_user
 
     if @answer.save
       redirect_to @question
@@ -11,6 +12,17 @@ class AnswersController < ApplicationController
       flash[:notice] = 'Your answer was not saved.'
       render 'questions/show'
     end
+  end
+
+  def destroy
+    @answer = Answer.find(params[:id])
+    if current_user == @answer.user
+      @answer.destroy
+      flash[:notice] = 'Your answer successfully deleted.'
+    else
+      flash[:notice] = 'You cannot delete this answer.'
+    end
+    redirect_to question_path(@answer.question)
   end
 
   private
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,25 +5,25 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'POST #create' do
     sign_in_user
+    let(:params) { { answer: attributes_for(:answer), question_id: question } }
+    let(:invalid_params) { { answer: attributes_for(:invalid_answer), question_id: question } }
 
     context 'with valid attributes' do
       it 'saves new answer in database' do
-        params = { answer: attributes_for(:answer), question_id: question }
         expect { post :create, params: params }.to change(question.answers, :count).by(1)
       end
       it 'redirects to show view' do
-        post :create, params: { answer: attributes_for(:answer), question_id: question }
+        post :create, params: params
         expect(response).to redirect_to assigns(:question)
       end
     end
 
     context 'with invalid attributes' do
       it 'does not save the question' do
-        params = { answer: attributes_for(:invalid_answer), question_id: question }
-        expect { post :create, params: params }.to_not change(Answer, :count)
+        expect { post :create, params: invalid_params }.to_not change(Answer, :count)
       end
       it 're-renders new view' do
-        post :create, params: { answer: attributes_for(:invalid_answer), question_id: question }
+        post :create, params: invalid_params
         expect(response).to render_template 'questions/show'
       end
     end
@@ -32,14 +32,25 @@ RSpec.describe AnswersController, type: :controller do
   describe 'DELETE #destroy' do
     sign_in_user
     let!(:answer_user) { create(:answer, question: question, user: @user) }
+    let!(:answer) { create(:answer, question: question) }
 
     context 'author deletes answer' do
       it 'deletes answer' do
         expect { delete :destroy, params: { id: answer_user } }.to change(Answer, :count).by(-1)
       end
-      it 'redirects to index view' do
-        delete :destroy, params: { id: answer }
-        expect(response).to redirect_to questions_path
+      it 'redirects to question' do
+        delete :destroy, params: { id: answer_user }
+        expect(response).to redirect_to answer_user.question
+      end
+    end
+
+    context 'non-author tries to delete question' do
+      it 'deletes answer' do
+        expect { delete :destroy, params: { id: answer } }.to_not change(Answer, :count)
+      end
+      it 'redirects to question' do
+        delete :destroy, params: { id: answer_user }
+        expect(response).to redirect_to answer.question
       end
     end
   end
