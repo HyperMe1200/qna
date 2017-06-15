64ff0abc8da2b3fbd41fc9a882081e4d2398b579
Andrey Pridatchenko <andrey.pridatchenko@rcntec.com>
2017-06-15 13:30:18 +0300

    Add multiple files

--- spec/acceptance/add_files_to_answers_spec.rb
+++ spec/acceptance/add_files_to_answers_spec.rb
@@ -13,12 +13,23 @@ feature 'Add files to answer', %q{
     visit question_path(question)
   end
 
-  scenario 'User add file when create answer', js: true do
+  scenario 'User add files when create answer', js: true do
     fill_in 'Input your answer', with: "answer_body"
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+
+    click_on 'Upload file'
+    within page.all('.nested-fields')[0] do
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    end
+
+    click_on 'Upload file'
+    within page.all('.nested-fields')[1] do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+
     click_on 'Post your answer'
     within '.answers' do
       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
     end
   end
 end
--- spec/acceptance/add_files_to_questions_spec.rb
+++ spec/acceptance/add_files_to_questions_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require_relative 'acceptance_helper'
 
 feature 'Add files to question', %q{
   In order to ilustrate my question
@@ -12,12 +12,22 @@ feature 'Add files to question', %q{
     visit new_question_path
   end
 
-  scenario 'User add file when ask question' do
+  scenario 'User add file when ask question', js: true do
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'Test body'
 
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'Upload file'
+    within page.all('.nested-fields')[0] do
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    end
+
+    click_on 'Upload file'
+    within page.all('.nested-fields')[1] do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+    save_and_open_screenshot
     click_on 'Create'
-    expect(page).to have_link 'spec_helper.rb', href: Attachment.last.file.url
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
   end
 end
