8854b861f2ffa2f0d848adf7387faba795d1feb6
Andrey Pridatchenko <andrey.pridatchenko@rcntec.com>
2017-06-15 21:18:49 +0300

    migrations refactoring

--- db/migrate/20170613185705_add_attachments_to_question.rb
+++
@@ -1,5 +0,0 @@
-class AddAttachmentsToQuestion < ActiveRecord::Migration[5.1]
-  def change
-    add_belongs_to :attachments, :question, index: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614061537) do
+ActiveRecord::Schema.define(version: 20170615180003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20170614061537) do
     t.string "file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "attachable_id"
+    t.integer "attachable_id"
     t.string "attachable_type"
     t.index ["attachable_id", "attachable_type"], name: "index_attachments_on_attachable_id_and_attachable_type"
   end
